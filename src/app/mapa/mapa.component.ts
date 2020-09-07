6e2b4a315314ee3b795b226acb898e79f3d38304
gabrielrosaless <gabrielpprosales@gmail.com>
2020-09-07 16:21:59 -0300

    4 files modified, 1 added

--- src/app/mapa/mapa.component.ts
+++ src/app/mapa/mapa.component.ts
@@ -1,65 +1,17 @@
-import { Component } from '@angular/core';
-// import { MouseEvent } from '@agm/core';
+import { Component, OnInit } from '@angular/core';
+import * as mapa from './mapa.js';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-map',
   templateUrl: './mapa.component.html',
   styleUrls: [ './mapa.component.css' ]
 })
 
-export class MapaComponent  {
-  title = 'My first AGM project';
-  lat = 51.678418;
-  lng = 7.809007;
-  // // google maps zoom level
-  // zoom: number = 8;
-  
-  // // initial center position for the map
-  // lat: number = 51.673858;
-  // lng: number = 7.815982;
+export class MapaComponent implements OnInit
+{
+  ngOnInit() {
+    mapa.initMap();    
+  }
 
-  // clickedMarker(label: string, index: number) {
-  //   console.log(`clicked the marker: ${label || index}`)
-  // }
-  
-  // mapClicked($event: MouseEvent) {
-  //   this.markers.push({
-  //     lat: $event.coords.lat,
-  //     lng: $event.coords.lng,
-  //     draggable: true
-  //   });
-  // }
-  
-  // markerDragEnd(m: marker, $event: MouseEvent) {
-  //   console.log('dragEnd', m, $event);
-  // }
-  
-  // markers: marker[] = [
-	//   {
-	// 	  lat: 51.673858,
-	// 	  lng: 7.815982,
-	// 	  label: 'A',
-	// 	  draggable: true
-	//   },
-	//   {
-	// 	  lat: 51.373858,
-	// 	  lng: 7.215982,
-	// 	  label: 'B',
-	// 	  draggable: false
-	//   },
-	//   {
-	// 	  lat: 51.723858,
-	// 	  lng: 7.895982,
-	// 	  label: 'C',
-	// 	  draggable: true
-	//   }
-  // ]
 }
 
-// just an interface for type safety.
-// interface marker {
-// 	lat: number;
-// 	lng: number;
-// 	label?: string;
-// 	draggable: boolean;
-// }
